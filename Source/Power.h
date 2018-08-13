c385ac98bbdfad6a1c592e7fdda94aea94c2fd76
Sebastián Varela <sebastian.varela@s3ba.net>
2018-08-13 11:41:24 +0200

    Create empty project

---
+++ Source/Power.h
@@ -0,0 +1,19 @@
+//
+//  Power.h
+//  Power
+//
+//  Created by Sebastián Varela on 13/8/18.
+//  Copyright © 2018 S3BA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Power.
+FOUNDATION_EXPORT double PowerVersionNumber;
+
+//! Project version string for Power.
+FOUNDATION_EXPORT const unsigned char PowerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Power/PublicHeader.h>
+
+
