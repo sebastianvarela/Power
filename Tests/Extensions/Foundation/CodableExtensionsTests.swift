cb8d586979d14ecf04331d7747ea8af406e484a1
Sebastián Varela <sebastian.varela@s3ba.net>
2018-09-20 10:10:30 +0200

    Change test because the order of keys are not guaranted

--- Tests/Extensions/Foundation/CodableExtensionsTests.swift
+++ Tests/Extensions/Foundation/CodableExtensionsTests.swift
@@ -4,9 +4,9 @@ import XCTest
 public class CodableExtensionsTests: XCTestCase {
     public func testJsonString() {
         let array = [1, 2, 3]
-        let dict = [1: "one", 2: "two"]
+        let dict = [1: "one"]
         
         XCTAssertEqual(array.jsonString, "[1,2,3]")
-        XCTAssertEqual(dict.jsonString, "{\"1\":\"one\",\"2\":\"two\"}")
+        XCTAssertEqual(dict.jsonString, "{\"1\":\"one\"}")
     }
 }
